422bcbaae910d95f165d481ba7b4ee219a0be8f7
deepin <changeforan@gmail.com>
2019-03-12 20:26:36 +0800

    dev

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 #import system things
 import tensorflow as tf
-import numpy as np
 import os
 
 #import helpers
@@ -9,14 +8,12 @@ from inputdata import Player
 
 
 if __name__ == "__main__":
-
     players = Player()
     sess = tf.InteractiveSession()
-    siamese = inference.siamese()
+    siamese = inference.Siamese()
     saver = tf.train.Saver()
-    model_ckpt = './model.meta'
+    model_ckpt = 'model/model.meta'
     if os.path.isfile(model_ckpt):
-        saver.restore(sess, './model')
-
+        saver.restore(sess, 'model/model')
     embed = siamese.o1.eval({siamese.x1: players.test.images})
     embed.tofile('embed.txt')
